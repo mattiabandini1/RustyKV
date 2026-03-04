c8eea1066e4415bbe2cea37626e412ab5c7d9f77
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-03-04 22:08:22 +0100

    feat: parse_command implemented

--- src/parser.rs
+++ src/parser.rs
@@ -6,5 +6,31 @@ pub enum Command {
 }
 
 pub fn parse_command(input: &str) -> Command {
+    let mut part = input.split_whitespace();
+    let operation = part.next();
 
+    match operation {
+        None => Command::Unknown,
+        Some(verb) => {
+            let lowercase_verb = verb.to_lowercase();
+
+            match lowercase_verb.as_str() {
+                "get" => {
+                    match part.next() {
+                        Some(key) => Command::Get(key.to_string()),
+                        None => Command::Unknown,
+                    }
+                },
+                "set" => {
+                    match (part.next(), part.next()) {
+                        (Some(key), Some(value)) => {
+                            Command::Set(key.to_string(), value.to_string())
+                        },
+                        _ => Command::Unknown,
+                    }
+                },
+                _ => Command::Unknown
+            }
+        }
+    }
 }
