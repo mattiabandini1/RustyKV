349880b4a38ddb72059983046f5f03b10153a232
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-03-04 22:17:37 +0100

    feat: main to test command_parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 mod parser;
+use parser::{parse_command, Command};
 
 fn main() {
-    println!("Rust-LSM started!");
+    println!("---Rust-LSM started!---");
+
+    let test_set = "    SET     user_3      password123     ";
+    let test_get = "get     user_3";
+    let test_fail = "DESTROY this db please!";
+
+    println!("Test SET: {:?}", parse_command(test_set));
+    println!("Test GET: {:?}", parse_command(test_get));
+    println!("Test FAIL: {:?}", parse_command(test_fail));
+
+    println!("---End test---")
 }
