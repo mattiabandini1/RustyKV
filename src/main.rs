5add39f7b762564ee0123269172e52973b87fc13
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-03-12 18:04:20 +0100

    feat: interactive database now working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::io::{self, Write};
+
 mod parser;
 mod memtable;
+
 use parser::{parse_command, Command};
 use memtable::MemTable;
 
@@ -7,35 +10,42 @@ fn main() {
 
     let mut db = MemTable::new();
 
-    let command_1 = "SET utente_99 super_segreto";
-    let command_2 = "GET utente_99";
+    println!("---Rust-LSM started---");
+    println!("Supported commands: SET <key> <value>, GET <key>, QUIT");
 
-    match parse_command(command_1) {
-        Command::Set(key, value) => {
-            db.set(key, value);
-            println!("key and value setted!");
-        },
-        Command::Get(key) => {
-            let result = db.get(&key);
-            println!("{:?}", result);
-        },
-        Command::Unknown => {
-            println!("Error! Command not valid.");
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Errors reading keyboards");
+
+        let clear_input = input.trim();
+
+        if clear_input == "QUIT" {
+            println!("Database shutdown in progress...");
+            break;
+        }
+
+        if clear_input.is_empty() {
+            continue;
+        }
+
+        match parse_command(clear_input) {
+            Command::Set(key, value) => {
+                db.set(key, value);
+                println!("key and value setted!");
+            },
+            Command::Get(key) => {
+                let result = db.get(&key);
+                println!("{:?}", result);
+            },
+            Command::Unknown => {
+                println!("Error! Command not valid.");
+            }
         }
     }
-
-    match parse_command(command_2) {
-        Command::Set(key, value) => {
-            db.set(key, value);
-            println!("key and value setted!");
-        },
-        Command::Get(key) => {
-            let result = db.get(&key);
-            println!("{:?}", result);
-        },
-        Command::Unknown => {
-            println!("Error! Command not valid.");
-        }
-    }
-
 }
